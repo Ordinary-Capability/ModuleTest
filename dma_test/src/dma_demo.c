cb16b3cd42a3bd30d80eaa9321bc6cb1efadec24
Allen Zheng <zq95521911@gmail.com>
2018-11-20 17:00:05 +0800

    dma case update

--- dma_test/src/dma_demo.c
+++ dma_test/src/dma_demo.c
@@ -1,5 +1,6 @@
 #include <rtdef.h>
 #include <rtdevice.h>
+#include <time.h>
 #include <mmu.h>
 
 #include <dma.h>
@@ -7,12 +8,17 @@
 
 
 #define ALIGN_DMA(x) ((x + 3)&(~3))
+#define MEM_POOL_SIZE 0x400000  //4M
+#define MAX_DMA_CHAN_NUM 5
 
+
+extern void do_gettimeofday(struct timeval *tv);
 
 struct rt_dma_device *gst_dma_device = RT_NULL;
 struct rt_completion dma_complete;
 
-
+static void *addr_src = RT_NULL;
+static void *addr_dst = RT_NULL;
 
 void dma_callback()
 {
@@ -52,67 +58,80 @@ struct dma_transfer *dma_transfer_construct(unsigned char *addr_src, unsigned ch
     pst_dma_transfer->dst_inc_mode = DW_DMA_SLAVE_INC;
     pst_dma_transfer->complete_callback = dma_callback;
     pst_dma_transfer->complete_para = RT_NULL;
-    pst_dma_transfer->trans_len    = trans_len;
+    pst_dma_transfer->trans_len    = (trans_len>>2);
 
     return pst_dma_transfer;
     }
 
-/*
-void data_tranafer_via_dma(struct dma_transfer * pTransfer,  unsigned char *addr_src,
-                            unsigned char *addr_dst, int data_len)
+
+int mem_init()
 {
-    rt_uint32_t i = 0;
-
-    if(addr_src == RT_NULL || addr_dst == RT_NULL
-        || data_len == 0 || pTransfer == RT_NULL)
-    {
-        rt_kprintf("Invalid input para.\n");
-        return;
-        }
-
-
-
-    }
-*/
-int dma_m2m(int channel)
-{
-    struct dma_transfer * pst_dma_transfer = RT_NULL;
-    unsigned char *addr_src = RT_NULL;
-    unsigned char *addr_dst = RT_NULL;
-    int data_len = ALIGN_DMA(0x20000);
-
-    addr_src = (unsigned char*)rt_malloc(data_len);
+    addr_src = rt_malloc(MEM_POOL_SIZE);
     if(addr_src == RT_NULL)
     {
         rt_kprintf("Rt malloc mem for addr_src fail.\n");
-        goto Error;
+        return -1;
         }
-    addr_dst = (unsigned char*)rt_malloc(data_len);
+
+    rt_memset(addr_src, 0, MEM_POOL_SIZE);
+    addr_dst = rt_malloc(MEM_POOL_SIZE);
     if(addr_dst == RT_NULL)
     {
         rt_kprintf("Rt malloc mem for addr_dst fail.\n");
-        goto Error;
+        return -1;
         }
+    
+    rt_memset(addr_dst, 0, MEM_POOL_SIZE);
+    return 0;
+    }
 
-    pst_dma_transfer = dma_transfer_construct(addr_src, addr_dst,
-                                            data_len>>2, channel);
+
+
+void src_buffer_build(unsigned char * addr_src, rt_uint32_t len)
+{
+    int i=0;
+    for(i=0; i<len; i++){*(addr_src+i) = i;}
+    mmu_clean_dcache((rt_uint32_t)addr_src, len);
+    return;
+    }
+
+int dst_buffer_varify(void *addr_src, void *addr_dts, rt_uint32_t len)
+{
+    mmu_invalidate_dcache((rt_uint32_t)addr_dts, len);
+    if(rt_memcmp(addr_src, addr_dts, len))return -1;
+
+    return 0;
+    }
+
+
+int dma_m2m_copy(int channel, rt_uint32_t data_len)
+{
+    struct dma_transfer * pst_dma_transfer = RT_NULL;
+    struct timeval t_start, t_end;
+    rt_int32_t delta = 0;
+    unsigned int i=0;
+
+    rt_kprintf("================= DMA channel %d m2m test ========\n", channel);
+    pst_dma_transfer = dma_transfer_construct((unsigned char*)addr_src, 
+                                                (unsigned char*)addr_dst,
+                                                data_len, channel);
     if(pst_dma_transfer == RT_NULL)
     {
         rt_kprintf("Construct dma transfer fail.\n");
-        goto Error;
+        return -1;
         }
 
     gst_dma_device =  (struct rt_dma_device*)rt_device_find("fh_dma0");
     if(gst_dma_device == RT_NULL)
     {
         rt_kprintf("Can not find device: fh_dma0.\n");
-        goto Error;
+        return -1;
         }
 
     if(gst_dma_device->ops->init(gst_dma_device))
     {
         rt_kprintf("Rt dma device init fail.\n");
-        goto Error;
+        return -1;
         }
 
     if(gst_dma_device->ops->control(gst_dma_device,
@@ -120,60 +139,93 @@ int dma_m2m(int channel)
                                     RT_NULL))
     {
         rt_kprintf("Open dma device fail.\n");
-        goto Error;
+        rt_free(pst_dma_transfer);
+        gst_dma_device->ops->control(gst_dma_device,
+                                        RT_DEVICE_CTRL_DMA_CLOSE,
+                                        pst_dma_transfer);
+        return -1;
         }
+
+
     if(gst_dma_device->ops->control(gst_dma_device,
                                     RT_DEVICE_CTRL_DMA_REQUEST_CHANNEL,
                                     pst_dma_transfer))
     {
-        rt_kprintf("Request dma device channel fail.\n");
-        goto Close;
+        rt_kprintf("Request dma device channel %d fail.\n", channel);
+        rt_free(pst_dma_transfer);
+        gst_dma_device->ops->control(gst_dma_device,
+                                        RT_DEVICE_CTRL_DMA_CLOSE,
+                                        pst_dma_transfer);
+        return -1;
         }
 
-   
-    /*Data transfer*/
-    unsigned int i=0;
-    rt_kprintf("Start data transfer...\n");
-    for(i=0; i<data_len; i++){addr_src[i] = i;}
-    mmu_clean_dcache((rt_uint32_t)addr_src, data_len);
     rt_completion_init(&dma_complete);
+    do_gettimeofday(&t_start);
     gst_dma_device->ops->control(gst_dma_device,
                                     RT_DEVICE_CTRL_DMA_SINGLE_TRANSFER,
                                     pst_dma_transfer); 
     rt_completion_wait(&dma_complete, RT_WAITING_FOREVER);
-    rt_kprintf("Dst memory content:\n");
-    for(i=0; i<0xff; i++)
-    {
-        rt_kprintf("0x%x", addr_dst[i]);
-        }
-
+    do_gettimeofday(&t_end);
+    delta = 1000000*(t_end.tv_sec - t_start.tv_sec) + t_end.tv_usec - t_start.tv_usec;
+    rt_kprintf("Mem copy size 0x%x bytes, Time consume: %d ms.\n", data_len, delta);
 
     if(gst_dma_device->ops->control(gst_dma_device,
                                     RT_DEVICE_CTRL_DMA_RELEASE_CHANNEL,
                                     pst_dma_transfer))
     {
-        rt_kprintf("Release dma device channel fail.\n");
-        goto Error;
+        rt_kprintf("Release dma device channel %d fail.\n", channel);
+        rt_free(pst_dma_transfer);
+        gst_dma_device->ops->control(gst_dma_device,
+                                        RT_DEVICE_CTRL_DMA_CLOSE,
+                                        pst_dma_transfer);
+        return -1;
         }
 
-Close:
-    if(gst_dma_device->ops->control(gst_dma_device,
-                                    RT_DEVICE_CTRL_DMA_CLOSE,
-                                    pst_dma_transfer))
-    {
-        rt_kprintf("Close dma device fail.\n");
-        goto Error;
-        }
-Error:
-    if(addr_src)rt_free(addr_src);
-    if(addr_dst)rt_free(addr_dst);
-    if(pst_dma_transfer)rt_free(pst_dma_transfer);
-    //if(gst_dma_device)rt_free(gst_dma_device);
     return 0;
-
-
     }
 
+
+
+
+int dma_m2m()
+{
+    rt_uint32_t copy_size_set[] = {0x4, 0x1000, 0x100000, 0x400000};
+    int i=0, j=0, ret=0,flag=0;
+
+    if(mem_init())
+    {
+        rt_kprintf("Mem poor init fail.\n");
+        goto _error;
+        }
+
+    for(i=0; i<=MAX_DMA_CHAN_NUM; i++)
+    {
+        flag = 0;
+        for(j=0; j<sizeof(copy_size_set)/4 && flag==0; j++)  
+        {
+            src_buffer_build(addr_src, copy_size_set[j]);
+            if(dma_m2m_copy(i, copy_size_set[j]) != 0)
+            {
+                rt_kprintf("DMA channel %d test fail.\n", i);
+                flag=1;
+                continue;
+                }
+            if(dst_buffer_varify(addr_src, addr_dst, copy_size_set[j]))
+            {
+                rt_kprintf("Dst buffer varify fail.\n");
+                goto _error;
+                }
+            }
+
+        }
+
+    return 0;
+
+_error:
+    if(addr_src)rt_free(addr_src);
+    if(addr_dst)rt_free(addr_dst);
+    return -1;
+    }
 
 #ifdef RT_USING_FINSH
 #include <finsh.h>
