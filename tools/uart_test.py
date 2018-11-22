6246560d57027a391f19d33977dd7b0aef07f456
Allen Zheng <zq95521911@gmail.com>
2018-11-22 19:04:33 +0800

    add uart test case

---
+++ tools/uart_test.py
@@ -0,0 +1,60 @@
+#/usr/bin/env python
+
+
+
+import os
+import sys
+import time
+
+import serial
+
+config_args = [[57600, 8, 'N', 1],
+                [115200, 8, 'N', 1],
+                [115200, 7, 'N', 1],
+                [115200, 8, 'N', 2],
+                [115200, 8, 'E', 1],
+                ]
+                
+bytes_pattern = bytearray(1024)
+def init_bytes_pattern(data_bits):
+    global bytes_pattern
+    for i in range(1024):
+        if data_bits==8:
+            bytes_pattern[i] = (i%256)
+        if data_bits==7:
+            bytes_pattern[i] = (i%256) & 0x7f
+        if data_bits==6:
+            bytes_pattern[i] = (i%256) & 0x3f
+        if data_bits==5:
+            bytes_pattern[i] = (i%256) & 0x1f
+
+if __name__ == "__main__":
+    port = sys.argv[1]
+    
+
+    for config in config_args:
+        print ("Test UART with config {}".format(config))
+        init_bytes_pattern(config[1])
+        s = serial.serial_for_url(port, *config, timeout=5)
+        if not s.isOpen:
+            print ("Open %s fail."%port)
+            sys.exit(-1)
+        s.write(bytes_pattern)
+        r = s.read(1024)
+        if not r:
+            print ("Recieve data from %s fail. \nUART test Fail."%port)
+            s.close()
+            sys.exit(-1)
+        if r != bytes_pattern:
+            print (repr(r))
+            print (len(r))
+            print ("Recieved data does not match to pattern data.\n UART test fail.")
+            s.close()
+            sys.exit(-1)
+        
+        time.sleep(3) #wait peer to configure and re-open uart               
+
+        s.close()
+
+
+
--- uart_test/src/uart_test.c
+++ uart_test/src/uart_test.c
@@ -5,6 +5,8 @@
 
 
 
+#define DATA_LEN 1024
+
 #define RT_SERIAL_CONFIG_DEFAULT           \
 {                                          \
     BAUD_RATE_115200, /* 115200 bits/s */  \
@@ -17,6 +19,54 @@
     0                                      \
 } 
 
+/* Test start with waiting for a solid bytes pattern. if the bytes recieved not
+ * match to the pattern, then test endwith fail-print info. Peer will occur some
+ * TIMEOUT issue to indicate test has failed. Otherwise, if the expected bytes
+ * revieved, then send the same bytes pattern to the peer and wait for new bytes
+ * coming in with another uart configuration. if peer recieves bytes correctly,
+ * start sending patten bytes again with another uart configuration which is match
+ * to the local one.
+ *
+ * Author: Zhengk315
+ */
+
+
+
+static unsigned char g_pattern[DATA_LEN] = {0};
+
+struct serial_configure g_uart_conf_set[] = {
+    {BAUD_RATE_57600, DATA_BITS_8, STOP_BITS_1, PARITY_NONE, BIT_ORDER_LSB, NRZ_NORMAL, RT_SERIAL_RB_BUFSZ, 0},
+    {BAUD_RATE_115200, DATA_BITS_8, STOP_BITS_1, PARITY_NONE, BIT_ORDER_LSB, NRZ_NORMAL, RT_SERIAL_RB_BUFSZ, 0},
+    {BAUD_RATE_115200, DATA_BITS_7, STOP_BITS_1, PARITY_NONE, BIT_ORDER_LSB, NRZ_NORMAL, RT_SERIAL_RB_BUFSZ, 0},
+    {BAUD_RATE_115200, DATA_BITS_8, STOP_BITS_2, PARITY_NONE, BIT_ORDER_LSB, NRZ_NORMAL, RT_SERIAL_RB_BUFSZ, 0},
+    {BAUD_RATE_115200, DATA_BITS_8, STOP_BITS_1, PARITY_EVEN, BIT_ORDER_LSB, NRZ_NORMAL, RT_SERIAL_RB_BUFSZ, 0},
+    };
+
+
+void init_transfer_pattern(int data_bits)
+{
+   int i = 0;
+   switch(data_bits)
+    {
+        case DATA_BITS_8:
+           for(i=0; i<DATA_LEN; i++) g_pattern[i] = i;
+           return;
+        case DATA_BITS_7:
+           for(i=0; i<DATA_LEN; i++) g_pattern[i] = i&0x7f;
+           return;
+        case DATA_BITS_6:
+           for(i=0; i<DATA_LEN; i++) g_pattern[i] = i&0x3f ;
+           return;
+        case DATA_BITS_5:
+           for(i=0; i<DATA_LEN; i++) g_pattern[i] = i&0x1f;
+           return;
+        default:
+           for(i=0; i<DATA_LEN; i++) g_pattern[i]=i;
+        }
+    //rt_kprintf("UART transfer bytes pattern:\n");
+    //for(i=0; i<DATA_LEN; i++)rt_kprintf("0x%x", g_pattern[i]); 
+    //rt_kprintf("\n");
+    }
 
 struct rt_semaphore rx_sem;
 
@@ -27,69 +77,119 @@ static rt_err_t uart_intput(rt_device_t dev, rt_size_t size)
     }
 
 
-void uart_test()
+int uart_config(const char *name, struct serial_configure *conf)
 {
-  rt_device_t uart_dev = RT_NULL;
-  struct rt_serial_device *serial;
-  char send_data[] = "HelloWorld!\n";
-  char recv_data[10] = {0};
-  rt_err_t ret = 0;
-  int i = 0, size = 0;
+    const char *device_name = name;
+    rt_device_t uart_dev = RT_NULL;
+    struct rt_serial_device *serial;
+    int ret=0;
 
-
-    uart_dev = rt_device_find("uart1");
+    rt_kprintf("Config %s baudrate:%d databits:%d stopbits:%d parity:%d \n",
+                name, conf->baud_rate, conf->data_bits, conf->stop_bits, conf->parity);
+    uart_dev = rt_device_find(name);
     if(uart_dev == RT_NULL)
     {
-        rt_kprintf("Can not find device uart1.\n");
-        return;
+        rt_kprintf("Can not find device %s.\n", name);
+        return -1;
         }
 
     if(rt_device_set_rx_indicate(uart_dev, uart_intput) != RT_EOK)
     {
         rt_kprintf("Set rx indicate fail.\n"); 
+        return -1;
         }
 
     serial = (struct rt_serial_device *)uart_dev;
-	(serial->config).baud_rate = 57600;
-    if(serial->ops->configure(serial, &(serial->config)) != RT_EOK)
+    if(serial->ops->configure(serial, conf) != RT_EOK)
 	{
-		rt_kprintf("Config uart1 fail.\n");
+		rt_kprintf("Config %s fail.\n", name);
+        return -1;
 		}
 
+    return 0;
+    }
+
+
+int uart_transfer(const char *name)
+{
+    rt_device_t uart_dev = RT_NULL;
+    char recv_data[DATA_LEN] = {0};
+    int count = 0, heel=0, i=0, j=0, ret;
+
+    uart_dev = rt_device_find(name);
+    if(uart_dev == RT_NULL)
+    {
+        rt_kprintf("Can not find device %s.\n", name);
+        return -1;
+        }
     if(rt_device_open(uart_dev, RT_DEVICE_OFLAG_RDWR|RT_DEVICE_FLAG_INT_RX) != RT_EOK)
     {
         rt_kprintf("Open device fail.\n");
-        return;
+        return -1;
         }
-
-   
     rt_sem_init(&rx_sem, "rx", 0, 0);
+    if(rt_sem_take(&rx_sem, 1000)<0)
+    {
+        rt_kprintf("No data recieved.\n");
+        rt_device_close(uart_dev);
+        return -1;
+        }
+    count = rt_device_read(uart_dev, 0, &recv_data, DATA_LEN);
+    heel = DATA_LEN - count;
+    while(heel>0 && rt_sem_take(&rx_sem, 10)==0)
+    {
+        count = count + rt_device_read(uart_dev, 0, &recv_data[count], DATA_LEN-count);
+        heel = DATA_LEN - count;
+        }
+
+    if(rt_memcmp(recv_data, g_pattern, DATA_LEN) != 0)
+    {
+        for(i=0; i<DATA_LEN; i++)
+            rt_kprintf("0x%x",recv_data[i]);
+        rt_kprintf("\n");
+        rt_kprintf("Recived data is uncorrect.\n");
+        rt_device_close(uart_dev);
+        return -1;
+        }
+    if(rt_device_write(uart_dev, 0, g_pattern, DATA_LEN) != DATA_LEN)
+    {
+        rt_kprintf("Send data error.\n");
+        rt_device_close(uart_dev);
+        return -1;
+        }
+    rt_thread_delay(10);
+    rt_device_close(uart_dev);
     
-
-    char ch;
-    while(1)
-    {
-        if (rt_sem_take(&rx_sem, RT_WAITING_FOREVER) != RT_EOK) continue;
-        while(rt_device_read(uart_dev, 0, &ch, 1) == 1)
-        {
-            rt_kprintf("%c", ch);
-            }
-    //rt_device_write(uart_dev, 0, send_data, strlen(send_data));
-        }
-
-
-    if(rt_device_close(uart_dev) != RT_EOK)
-    {
-        rt_kprintf("Close device fail.\n");
-        return;
-        }
-
-    rt_kprintf("Uart test exit.\n");
-  
-
-    return;
+    return 0;
     }
 
+
+
+int uart_test(const char *name)
+{
+    rt_uint32_t i, n;
+
+    rt_kprintf("=============== UART %s test start ===========\n", name);
+    n = sizeof(g_uart_conf_set)/sizeof(struct serial_configure);
+    for(i=0; i<n; i++)
+    {
+        init_transfer_pattern(g_uart_conf_set[i].data_bits);
+        if(0 != uart_config(name, &g_uart_conf_set[i]))
+        {
+            rt_kprintf("Uart %s test fail.\n", name);
+            return -1;
+            }
+
+        if(0 != uart_transfer(name))
+        {
+            rt_kprintf("UART %s data transfer failed.\n", name);
+            return -1;
+            }
+        }
+    rt_kprintf("=============== UART %s test end ===========\n", name);
+
+    return 0;
+    }
 
 
 #ifdef RT_USING_FINSH
